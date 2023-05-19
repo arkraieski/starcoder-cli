c2cf64da4d8520e96fde8b0946d6000fc894ba75
Alexander Kraieski <arkraieski@gmail.com>
2023-05-18 23:09:56 -0400

    fixed not waiting for API key input

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,4 @@
-#! /usr/bin/env node
+#!/usr/bin/env node
 
 const yargs = require("yargs");
 const fs = require('fs');
@@ -6,55 +6,52 @@ const os = require('os');
 const path = require('path');
 
 const { requestCompletion, createEnvFile } = require("./utils");
-const { stderr } = require("process");
 
 const usage = "\nUsage: starcode [options] <code to complete>";
-    
+
 yargs
-    .usage(usage)  
-    .option("f", {alias:"file", 
-      describe: "Specify a file to get text input from", 
-      type: "string", 
-      demandOption: false })  
-    .help();                                                                                                  
-      
-const args = yargs.argv;
+  .usage(usage)
+  .option("f", {
+    alias: "file",
+    describe: "Specify a file to get text input from",
+    type: "string",
+    demandOption: false
+  })
+  .help();
 
-if (process.argv.length <= 2) {
-    yargs.showHelp();
-    process.exit(1);
-  }
+(async () => {
+  const args = await yargs.parse();
 
-const envFile = path.join(os.homedir(), '.starcoder-cli-env');
-const envResult = require('dotenv').config({ path: envFile });
+  if (!args.file) {
+    if (args._.length === 0) {
+      yargs.showHelp();
+      process.exit(1);
+    }
 
-if(envResult.error){
-    (async () => {
-        await createEnvFile();
-      })();
-}
+    const envFile = path.join(os.homedir(), '.starcoder-cli-env');
+    const envResult = require('dotenv').config({ path: envFile });
 
-const apiKey = process.env.HF_API_KEY
+    if (envResult.error) {
+      await createEnvFile(envFile);
+    }
 
-if (args.f) {
-    // Read the file and process its contents
-    const fileContents = fs.readFileSync(args.f, 'utf8');
-    requestCompletion(fileContents, apiKey).then((result) => {
-        console.log(result.generated_text);
-      }).catch((error) => {
-        console.error(error);
-      });
+    const apiKey = process.env.HF_API_KEY;
+
+    const result = await requestCompletion(args._[0], apiKey);
+    console.log(result.generated_text);
   } else {
-    //console.log(args._[0]);
-    // Use the provided string input
-    requestCompletion(args._[0], apiKey).then((result) => {
-        console.log(result.generated_text);
-      }).catch((error) => {
-        console.error(error);
-      });
+    // Read the file and process its contents
+    const fileContents = fs.readFileSync(args.file, 'utf8');
+    const envFile = path.join(os.homedir(), '.starcoder-cli-env');
+    const envResult = require('dotenv').config({ path: envFile });
+
+    if (envResult.error) {
+      await createEnvFile(envFile);
+    }
+
+    const apiKey = process.env.HF_API_KEY;
+
+    const result = await requestCompletion(fileContents, apiKey);
+    console.log(result.generated_text);
   }
-
-
-
-
-// const res =  requestCompletion(hf, text);
+})();
